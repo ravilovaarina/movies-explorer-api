8922e4cb932a9e96ad572a445d87fca5ddf01bfe
Arina Ravilova <ravilova.arina06@gmail.com>
2024-01-13 09:40:34 +0300

    fix: неправльное регулярное выражение

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const allowedUrl = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+const allowedUrl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const errorMessages = {
   image: 'Некорректный формат ссылки на картинку',
